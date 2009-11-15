801c553e8ddfaebd0f837b7cd1cc4e8239dce557
Frederik Fix <fred@stemcel.co.uk>
2009-11-15 15:25:07 +0100

    added install hook for config file

--- install.rb
+++ install.rb
@@ -1,1 +1,4 @@
-# Install hook code here
+config_path = File.join('config', 'adaptive_pay.yml')
+unless File.exist?(File.join(RAILS_ROOT, config_path))
+  FileUtils.cp File.join(File.dirname(__FILE__), config_path), File.join(RAILS_ROOT, config_path)
+end
